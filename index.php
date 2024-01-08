18d7ce449f338c1659fa896771546b92701a2899
xPiLoYx <rexs.llemel333@gmail.com>
2024-01-09 03:33:12 +0800

    update homepage css

--- index.php
+++ index.php
@@ -61,7 +61,8 @@
                     <div id="waterAlertLumbayao">
                         <!-- Alert message will be updated dynamically here -->
                     </div>
-                    <div class="col-lg-5 offset-lg-0 mb-4">
+                    <div class="row justify-content-center" style="margin-bottom: 0px;padding-bottom: 31px;">
+                        <div class="col-lg-5 offset-lg-0 mb-4">
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
                                     <h6 class="text-primary fw-bold m-0">Valencia City Weather Forecast</h6>
@@ -71,6 +72,7 @@
                                 </script>
                             </div>
                         </div>
+                    </div> 
                     <div class="row justify-content-center" style="margin-bottom: 0px;padding-bottom: 31px;">
                         <div class="col-lg-5 col-xl-4">
                             <div class="card shadow mb-4 card-size">
@@ -92,7 +94,7 @@
                                 </div>
                             </div>
                         </div>
-                    <div class="row justify-content-center" style="margin-bottom: 0px;padding-bottom: 31px;">
+                        <div class="row justify-content-center" style="margin-bottom: 0px;padding-bottom: 31px;">
                         <div>
                             <div class="card shadow">
                                 <div class="card-header py-3">

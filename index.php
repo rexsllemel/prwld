65bf7dd6e4c28c12d76bb67c3e54f24505c2e30a
xPiLoYx <rexs.llemel333@gmail.com>
2024-01-09 05:10:11 +0800

    center text

--- index.php
+++ index.php
@@ -73,10 +73,10 @@
                             </div>
                         </div>
                     </div> 
-                    <div class="row justify-content-center text-center" style="margin-bottom: 0px;padding-bottom: 31px;">
+                    <div class="row justify-content-center text-center" style="margin-bottom: 0px;padding-bottom: 15px;">
                         <div class="col-lg-5 col-xl-4">
                             <div class="card shadow mb-4 card-size">
-                                <div class="card-header d-flex justify-content-between align-items-center" style="padding-bottom: 24px;">
+                                <div class="card-header d-flex justify-content-between align-items-center text-center" style="padding-bottom: 24px;">
                                     <h6 class="text-primary fw-bold m-0 text-center" >Pulangui Batangan Water Level Meter</h6>
                                 </div>
                                 <div class="card-body water-level" style="margin-top: -2px;padding-top: 2px;padding-bottom: 0px;margin-bottom: 4px;"><div class="text-center small padding-top"><span class="me-2"><i class="fas fa-circle safe-color"></i> Safe</span><span class="me-2"><i class="fas fa-circle warning-color"></i> Warning</span><span class="me-2"><i class="fas fa-circle danger-color"></i> Danger</span></div><script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -86,7 +86,7 @@
                     </div>
                      <div class="col-lg-5 col-xl-4">
                             <div class="card shadow mb-4 card-size">
-                                <div class="card-header d-flex justify-content-between align-items-center" style="padding-bottom: 24px;">
+                                <div class="card-header d-flex justify-content-between align-items-center text-center" style="padding-bottom: 15px;">
                                     <h6 class="text-primary fw-bold m-0 text-center">Pulangui Lumabayao Water Level Meter</h6>
                                 </div>
                                 <div class="card-body water-level" style="margin-top: -2px;padding-top: 2px;padding-bottom: 0px;margin-bottom: 4px;"><div class="text-center small padding-top"><span class="me-2"><i class="fas fa-circle safe-color"></i> Safe</span><span class="me-2"><i class="fas fa-circle warning-color"></i> Warning</span><span class="me-2"><i class="fas fa-circle danger-color"></i> Danger</span></div><script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>

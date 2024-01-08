5e0056c978ce1ea389cc1279d65c827ee7fa2a5d
xPiLoYx <rexs.llemel333@gmail.com>
2024-01-08 13:18:58 +0800

    adding api access

---
+++ assets/node2/node2.ino
@@ -0,0 +1,90 @@
+#include <ESP8266WiFi.h>
+
+#define TRIG_PIN 12
+#define ECHO_PIN 14
+const char* ssid     = "FAITH WiFi";
+const char* password = "";
+const char* host = "38.54.80.251";
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+
+void loop() {
+  // Triggering the ultrasonic sensor
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Reading the echo time
+  long duration = pulseIn(ECHO_PIN, HIGH);
+
+  // Calculating the distance
+  float distance = duration * 0.034 / 2;
+
+  // Displaying the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+ 
+
+
+    // This will send the request to the server
+ client.print(String("GET http://faithvpn.site/pulangui/assets/connect.php?") + 
+                          ("&distance=") + distance +
+                          " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 1000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+        
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
+
+  delay(23000); // You can adjust the delay based on your needs
+
+}

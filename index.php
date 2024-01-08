b1024511a83cd03eec4643261570f05ccceb500d
xPiLoYx <rexs.llemel333@gmail.com>
2024-01-09 05:07:29 +0800

    text-center

--- index.php
+++ index.php
@@ -61,11 +61,11 @@
                     <div id="waterAlertLumbayao">
                         <!-- Alert message will be updated dynamically here -->
                     </div>
-                    <div class="row justify-content-center" style="margin-bottom: 0px;padding-bottom: 15px;">
+                    <div class="row justify-content-center text-center" style="margin-bottom: 0px;padding-bottom: 15px;">
                         <div class="col-lg-5 offset-lg-0 mb-4">
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="text-primary fw-bold m-0">Valencia City Weather Forecast</h6>
+                                    <h6 class="text-primary fw-bold m-0 text-center">Valencia City Weather Forecast</h6>
                                 </div><a class="weatherwidget-io forecast" href="https://forecast7.com/en/7d86125d17/valencia-city/" data-label_1="VALENCIA" data-label_2="Pulangui Weather" data-theme="weather_one" >VALENCIA Pulangui Weather</a>
                                 <script>
                                 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
@@ -87,7 +87,7 @@
                      <div class="col-lg-5 col-xl-4">
                             <div class="card shadow mb-4 card-size">
                                 <div class="card-header d-flex justify-content-between align-items-center" style="padding-bottom: 24px;">
-                                    <h6 class="text-primary fw-bold m-0">Pulangui Lumabayao Water Level Meter</h6>
+                                    <h6 class="text-primary fw-bold m-0 text-center">Pulangui Lumabayao Water Level Meter</h6>
                                 </div>
                                 <div class="card-body water-level" style="margin-top: -2px;padding-top: 2px;padding-bottom: 0px;margin-bottom: 4px;"><div class="text-center small padding-top"><span class="me-2"><i class="fas fa-circle safe-color"></i> Safe</span><span class="me-2"><i class="fas fa-circle warning-color"></i> Warning</span><span class="me-2"><i class="fas fa-circle danger-color"></i> Danger</span></div><script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
                                 <div id="lumbayao_gauge" class="water-meter"></div>

c0d43579564226392d8e2cbc351194dbffa6e6f8
xPiLoYx <rexs.llemel333@gmail.com>
2024-01-09 13:10:26 +0800

    revert

--- assets/db_connect.php
+++ assets/db_connect.php
@@ -2,11 +2,9 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$passwordlocal = "admin@aclc";
 $database = "waterlevel";
 
 $conn = new mysqli($host, $username, $password, $database);
-$connLocal = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);

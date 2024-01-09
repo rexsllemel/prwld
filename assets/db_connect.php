da936b5594d767abce5997ff0e1123a2e94afabf
xPiLoYx <rexs.llemel333@gmail.com>
2024-01-09 13:08:07 +0800

    bug fix

--- assets/db_connect.php
+++ assets/db_connect.php
@@ -2,9 +2,10 @@
 $host = "localhost";
 $username = "root";
 $password = "";
+$passwordlocal = "admin@aclc";
 $database = "waterlevel";
 
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password || $passwordlocal, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
